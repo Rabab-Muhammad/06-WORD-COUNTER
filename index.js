d5ecaa245d896de3918d0a9fb5cf47fb890bff96
Rabab-Muhammad <160467570+Rabab-Muhammad@users.noreply.github.com>
2024-04-21 14:31:42 +0500

    push word-counter to github

---
+++ index.js
@@ -0,0 +1,23 @@
+#! /usr/bin/env node
+//Importing inquirer and chalk packages
+import inquirer from "inquirer";
+import chalk from "chalk";
+//Display a colourfull welcome message
+console.log(chalk.bold.cyanBright("\n \t\t WELCOME TO - 'WORD COUNTER'"));
+console.log("=".repeat(60));
+//Prompt the user to enter a sentence
+let answers = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: "Enter a Sentence:"
+    }
+]);
+//Trimming the sentence and spiliting it into words based on "spaces".
+let words = answers.sentence.trim().split(" ");
+//Analysis of user input sentence
+console.log("=".repeat(60));
+console.log(chalk.bold("- Sentence Words:"));
+console.log(words);
+console.log(chalk.bold(`\n -Word Count: ${chalk.bold.redBright(words.length)}`));
+console.log("=".repeat(60));
